9da5567263f72d225474ee748e0cfd4b66c5316e
Aditya <ratheeshaditya@gmail.com>
2025-09-13 09:34:52 +0200

    basic record handler and model

--- rustfly/src/main.rs
+++ rustfly/src/main.rs
@@ -1,53 +1,34 @@
+use std::collections::HashMap;
+// use crate::model::ColumnField;
+mod model;
 
- use std::collections::HashMap;
-#[derive(Debug)]
+use model::{ColumnField, Model};
 
-struct Field{
-    name: String,
-    field_type: String,
-    is_required: bool,
-    is_unique: bool,
-}
-
-#[derive(Debug)]
-struct ModelSchema{
-    fields: Vec<Field>,
-}
-#[derive(Debug)]
-struct Model{
-    class_name: String,
-    schema: ModelSchema,
-}
-
-impl Model {
-    fn define_schema(params: HashMap <String,Field>) -> ModelSchema {
-       let mut fields: Vec<Field> = Vec::new();
-
-       for (_, field) in params.into_iter() {
-        fields.push(field)
-       }
-
-       ModelSchema { fields }
-    }
-
-    fn new(name: String, params: HashMap <String,Field>) -> Model {
-        let model_schema: ModelSchema = Model::define_schema(params);
-
-        Model {
-            class_name: name.to_string(),
-            schema: model_schema
-        }
-    }
-
-
-
-}
 fn main() {
-   
     let mut user_fields = HashMap::new();
-    user_fields.insert("user".to_string(), Field { name:"user".to_string(), field_type: "int".to_string(), is_required: true, is_unique: true });
-    user_fields.insert("email".to_string(), Field { name: "email".to_string(), field_type: "string".to_string(), is_required: true, is_unique: true });
+    user_fields.insert("user".to_string(), ColumnField {
+        name: "user".to_string(),
+        field_type: "int".to_string(),
+        is_required: true,
+        is_unique: true,
+    });
+    user_fields.insert("email".to_string(), ColumnField {
+        name: "email".to_string(),
+        field_type: "string".to_string(),
+        is_required: true,
+        is_unique: true,
+    });
 
-    let user_model = Model::new("User".to_string(), user_fields);
+    let mut user_model = Model::new("User".to_string(), user_fields);
     println!("{:#?}", user_model);
+
+    let mut record = HashMap::new();
+    record.insert("user".to_string(), "123".to_string());
+    record.insert("email".to_string(), "test@example.com".to_string());
+
+    user_model.create_record(record);
+    user_model.show_records();
+        println!("{:#?}", user_model);
+    // user_model.record_handler.find("email".to_string(), "test@example.com".to_string(), &user_model.schema);
+    user_model.record_handler.find_all("email".to_string(), "test@example.com".to_string(), &user_model.schema);
 }
