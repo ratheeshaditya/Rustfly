22697bdf7372c7175f4028dbcd30919d14450021
Aditya <ratheeshaditya@gmail.com>
2025-09-14 09:34:58 +0200

    implemented basic where functionality

--- rustfly/src/main.rs
+++ rustfly/src/main.rs
@@ -20,15 +20,25 @@ fn main() {
     });
 
     let mut user_model = Model::new("User".to_string(), user_fields);
-    println!("{:#?}", user_model);
+    // println!("{:#?}", user_model);
 
     let mut record = HashMap::new();
     record.insert("user".to_string(), "123".to_string());
     record.insert("email".to_string(), "test@example.com".to_string());
-
     user_model.create_record(record);
-    user_model.show_records();
-        println!("{:#?}", user_model);
+
+    // user_model.show_records();
+        // println!("{:#?}", user_model);
     // user_model.record_handler.find("email".to_string(), "test@example.com".to_string(), &user_model.schema);
-    user_model.record_handler.find_all("email".to_string(), "test@example.com".to_string());
+    // user_model.record_handler.find_all("email".to_string(), "test@example.com".to_string());
+
+    let mut new_record = HashMap::new();
+    new_record.insert("user".to_string(), "123456".to_string());
+    new_record.insert("email".to_string(), "test@example.com".to_string());
+    user_model.create_record(new_record);
+    
+    user_model.record_handler.find_where(
+        &HashMap::from([("email".to_string(),"test@example.com".to_string())])
+    )
+    
 }
